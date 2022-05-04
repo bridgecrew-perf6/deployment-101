73566c6058f3902cb8b54d50c2ae5a44575c9a10
Benny Mendoza <bennybc9387@gmail.com>
2022-05-03 21:06:17 -0600

    fix path

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -8,3 +10,10 @@ app.use(express.json())
 app.use(cors())
 
 app.listen(PORT, () => console.log(`up on ${PORT}`))
+
+
+app.get('*', (req, res) => {
+    const htmlFilePath = path.resolve(__dirname, 'landingPage.html');
+  
+    res.sendFile(htmlFilePath);
+  });
