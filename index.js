c0c9f8f49433f80d32a885932953a8f6272e882c
Benny Mendoza <bennybc9387@gmail.com>
2022-05-03 20:59:07 -0600

    second push

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = process.env
+const {PORT} = process.env
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
+app.listen(PORT, () => console.log(`up on ${PORT}`))
